6681cddef04a2e32bd6e10d1b302fee704a9e059
wblazej <blazej.wrzosok@gmail.com>
2021-03-16 20:39:22 +0100

    move formating date switchers to config file

--- lib/config.py
+++ lib/config.py
@@ -105,3 +105,28 @@ class Config:
         "Dodawanie reakcji",
         "Wyświetlanie kanałów"
     ]
+
+    # Date formating
+    DAYS_OF_WEEK                    = {
+        0: "poniedziałek",
+        1: "wtorek",
+        2: "środa",
+        3: "czwartek",
+        4: "piątek",
+        5: "sobota",
+        6: "niedziela"
+    }
+    MONTHS                          = {
+        1: "Styczeń",
+        2: "Luty",
+        3: "Marzec",
+        4: "Kwiecień",
+        5: "Maj",
+        6: "Czerwiec",
+        7: "Lipiec",
+        8: "Sierpień",
+        9: "Wrzesień",
+        10: "Październik",
+        11: "Listopad",
+        12: "Grudzień"
+    }
--- lib/format_date.py
+++ lib/format_date.py
@@ -1,36 +1,12 @@
 import pytz
 from datetime import datetime
+from lib.config import Config
 
 def weekday_string(weekday):
-    switcher = {
-        0: "poniedziałek",
-        1: "wtorek",
-        2: "środa",
-        3: "czwartek",
-        4: "piątek",
-        5: "sobota",
-        6: "niedziela"
-    }
-
-    return switcher.get(weekday, "Invalid weekday")
+    return Config.DAYS_OF_WEEK.get(weekday, "Invalid weekday")
 
 def month_string(month):
-    switcher = {
-        1: "Styczeń",
-        2: "Luty",
-        3: "Marzec",
-        4: "Kwiecień",
-        5: "Maj",
-        6: "Czerwiec",
-        7: "Lipiec",
-        8: "Sierpień",
-        9: "Wrzesień",
-        10: "Październik",
-        11: "Listopad",
-        12: "Grudzień"
-    }
-
-    return switcher.get(month, "Invalid month")
+    return Config.MONTHS.get(month, "Invalid month")
 
 def get_formated_warsaw_datetime():
     tz = pytz.timezone('Europe/Warsaw')
