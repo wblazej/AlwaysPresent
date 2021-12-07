a8309aca2fb35963fba95d4013eb70a9c62cf8c6
wblazej <blazej.wrzosok@gmail.com>
2021-12-07 16:35:44 +0100

    changed date and time formatting in logging

--- lib/logging.py
+++ lib/logging.py
@@ -1,10 +1,13 @@
-from lib.format_date import FormatDate
+from datetime import datetime
+
+def get_time():
+    return datetime.now().strftime("%A, %d %B %Y %H:%M:%S")
 
 class Logging:
     @staticmethod
     def error(content):
-        print(f"\033[1m\033[91mERROR\033[0m {content} | {FormatDate.get_formated_datetime()}")
+        print(f"\033[1m\033[91mERROR\033[0m {content} | {get_time()}")
 
     @staticmethod
     def info(content):
-        print(f"\033[94m\033[1mINFO\033[0m {content} | {FormatDate.get_formated_datetime()}")
+        print(f"\033[94m\033[1mINFO\033[0m {content} | {get_time()}")
